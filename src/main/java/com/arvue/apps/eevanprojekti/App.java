9170816f06645e9027cfac40aa8e5b3ee9e2a6ab
ubuntu <ubuntu@mideaas.novalocal>
2013-06-12 06:48:22 +0000

    initial commit

---
+++ src/main/java/com/arvue/apps/eevanprojekti/App.java
@@ -0,0 +1,21 @@
+package com.arvue.apps.eevanprojekti;
+
+import com.vaadin.addon.touchkit.ui.NavigationManager;
+import com.vaadin.addon.touchkit.ui.NavigationView;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.Component;
+
+@SuppressWarnings("serial")
+public class App extends UI {
+    @Override    protected void init(VaadinRequest request) {
+        Component c = new Main();
+        c.setSizeFull();
+        setContent(new NavigationManager(c));
+    }
+
+    public static class MideaasController {
+        public void componentCreated(NavigationView view) {}
+    }
+
+}
---
+++ src/main/java/com/arvue/apps/eevanprojekti/Main.java
@@ -0,0 +1,11 @@
+package com.arvue.apps.eevanprojekti;
+
+import org.vaadin.mideaas.MideaasNavigationView;
+
+public class Main extends MideaasNavigationView {
+
+    @Override
+    public void onBecomingVisible() {
+        super.onBecomingVisible();
+    }
+}
